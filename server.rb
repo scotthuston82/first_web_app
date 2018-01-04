7346750940ecc3d2088754c08f5d0ec636c749d6
Scott <scotthuston1982@gmail.com>
2018-01-04 15:50:57 -0500

    final commit

--- server.rb
+++ server.rb
@@ -7,3 +7,22 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['www.a.com', 'www.b.com', 'www.c.com', 'www.d.com', 'www.e.com']
+  erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
